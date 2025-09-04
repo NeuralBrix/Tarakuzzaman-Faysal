42580b3fe3b4a7c4b43bf764ecc62297f5a02ec5
iamfosu <faysal612519112@gmail.com>
2025-09-04 20:22:52 +0600

    encapsulation

--- oop_python/encap1.py
+++ oop_python/encap1.py
@@ -11,8 +11,22 @@ class house:
     def name(self):
         print(f"This will be the house for poor people")
 
+    def details(self):
+        print(self.__balance)    
+
 owner1 = house("Faysal")
 owner2 = house("Muhammad")
 
 owner1.who_designer()
 owner1.name()
+
+owner1.__balance = 1000
+owner2.__balance = 2000
+
+print(owner1.__dict__)
+print(owner1.__balance)
+print(owner2.__balance)
+# print(owner1.balance)
+# print(owner2.balance)
+
+owner1.details()
